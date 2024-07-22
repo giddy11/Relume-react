444fc283a542ef76484781b21138ce7b43179795
Edoghotu Gideon Azibaobuom <gideon.edoghotu@cyphercrescent.com>
2024-07-22 18:04:32 +0100

    impllemented

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,5 +1,6 @@
 import { Router } from 'express';
 import * as controller from '../controllers/authController.js';
+import Auth, { localVariables } from '../middleware/auth.js';
 const router = Router();
 
 /** POST Methods */
@@ -16,7 +17,7 @@ router.route('/user/:username').get(controller.getUser) // user with username
 
 
 /** PUT Methods */
-// router.route('/updateuser').put(Auth, controller.updateUser); // is use to update the user profile
+router.route('/updateuser').put(Auth, controller.updateUser); // is use to update the user profile
 // router.route('/resetPassword').put(controller.verifyUser, controller.resetPassword); // use to reset password
 
 
